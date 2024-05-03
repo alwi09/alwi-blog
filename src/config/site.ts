bc526686df20e80a5fa73b3fec04c45c99a877ba
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2024-05-03 14:22:38 +0700

    [FEATURE] add site header and main raw nav

---
+++ src/config/site.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  name: "AlwiBlog",
+  url: "https://alwiblog.vercel.app",
+  description:
+    "Alwi's Blog Creating with Next.js 14 using velite, tailwindcss and shadcn ui",
+  author: "Mohammad Alwi Irfani",
+  links: {
+    facebook: "https://www.facebook.com/art.Xfast",
+    instagram: "https://www.instagram.com/alwi_irfani470",
+    github: "https://github.com/alwi09",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
