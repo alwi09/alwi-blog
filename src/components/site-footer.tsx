e9322196b8d2eccdb295f69652163c2dc06d4464
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2024-05-09 01:13:18 +0700

    [CHANGE] change bg icon footer

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -13,17 +13,17 @@ export function SiteFooter() {
           href={`mailto:${process.env.NEXT_MY_EMAIL}`}
           target="_blank"
           rel="noreferrer"
-          className="flex h-6 w-6 items-center justify-center rounded-md hover:bg-muted ring-offset-background transition-colors dark:bg-foreground/45 bg-foreground/5"
+          className="flex h-6 w-6 items-center justify-center rounded-md hover:bg-muted ring-offset-background transition-colors dark:bg-foreground/15 bg-foreground/5"
         >
-          <Icons.email className="inline-block h-5 w-5" />
+          <Icons.email className="inline-block h-5 w-5 fill-current dark:text-foreground" />
         </Link>
         <Link
           href={`${siteConfig.links.linkedin}`}
           target="_blank"
           rel="noreferrer"
-          className="flex h-6 w-6 items-center justify-center rounded-md hover:bg-muted ring-offset-background transition-colors dark:bg-foreground/45 bg-foreground/5"
+          className="flex h-6 w-6 items-center justify-center rounded-md hover:bg-muted ring-offset-background transition-colors dark:bg-foreground/15 bg-foreground/5"
         >
-          <Icons.linkedin className="inline-block h-5 w-5" />
+          <Icons.linkedin className="inline-block h-5 w-5 fill-current dark:text-foreground" />
         </Link>
       </div>
     </footer>
