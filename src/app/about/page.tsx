a26144fe0cad5697fac46a8f0ff301818fad65c3
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2024-05-14 12:47:16 +0700

    [CHANGE] add CRUD Express mdx and change animation border

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,9 +19,9 @@ export default async function AboutPage() {
         </div>
       </div>
       <hr className="my-8" />
-      <div className="flex flex-col md:flex-row gap-8 items-center md:items-start">
-        <div className="min-w-48 max-w-48  md:min-h-60 md:min-w-60 flex flex-col gap-2">
-          <Avatar className="h-48 w-48 md:h-60 md:w-60 hover:border-4 hover:border-solid hover:scale-105 transition-all">
+      <div className="flex flex-col md:flex-row gap-8 items-center justify-center">
+        <div className="min-w-48 max-w-48 md:min-h-60 md:min-w-60 md:justify-center md:items-center flex flex-col gap-2">
+          <Avatar className="h-48 w-48 md:h-60 md:w-60 border-4 border-solid border-foreground/15 dark:border-foreground/50 hover:scale-105 transition-all">
             <AvatarImage
               src="/avatar.png"
               alt={siteConfig.author}
@@ -36,8 +36,8 @@ export default async function AboutPage() {
             Web Developer
           </p>
         </div>
-        <div className="flex w-full min-h-60 md:px-5 md:justify-start md:items-center">
-          <div className="w-80 h-fit border-4 border-border lg:w-96 border-rainbow">
+        <div className="flex min-w-48 min-h-60 md:px-5 md:justify-center md:items-center">
+          <div className="w-80 h-fit border-4 border-border border-solid lg:w-96 border-rainbow p-2 md:mb-16">
             <p className="text-base text-muted-foreground p-2">
               Hi There 🖐️, Thanks for coming by. I wrote my ramblings on tech
               and dev. like about Go, Java and Javascript. 📃
